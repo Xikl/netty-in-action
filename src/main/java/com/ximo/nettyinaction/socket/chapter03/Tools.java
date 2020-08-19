b80ec01f4b7b7e0e0d17e1624989e317593fae95
朱文赵 <1392936858@qq.com>
2020-08-19 23:05:54 +0800

    tcp的各个api的学习

---
+++ src/main/java/com/ximo/nettyinaction/socket/chapter03/Tools.java
@@ -0,0 +1,23 @@
+package com.ximo.nettyinaction.socket.chapter03;
+
+/**
+ * @author xikl
+ * @date 2020/8/19
+ */
+public class Tools {
+    public static int byteArrayToInt(byte[] b) {
+        return b[3] & 0xFF |
+                (b[2] & 0xFF) << 8 |
+                (b[1] & 0xFF) << 16 |
+                (b[0] & 0xFF) << 24;
+    }
+
+    public static byte[] intToByteArray(int a) {
+        return new byte[]{
+                (byte) ((a >> 24) & 0xFF),
+                (byte) ((a >> 16) & 0xFF),
+                (byte) ((a >> 8) & 0xFF),
+                (byte) (a & 0xFF)
+        };
+    }
+}
